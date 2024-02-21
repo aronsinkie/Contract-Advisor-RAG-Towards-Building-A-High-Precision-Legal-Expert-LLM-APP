ee134d9dd421afab77c51de9656ca5ac7d406b95
aronsinkie <birliesinkie2010@gmail.com>
2024-02-21 18:21:23 +0000

    Django

---
+++ contract_advisor/RAGchatbot/views.py
@@ -0,0 +1,130 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+#import openai
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import Chat
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+import os
+def create_qa_model():
+
+    # Load documents
+    loader = PyPDFLoader("/home/aron/Music/10acadamy/Week11/Contract-Advisor-RAG-Towards-Building-A-High-Precision-Legal-Expert-LLM-APP/Evaluationpdf/contract.pdf")
+    documents = loader.load()
+
+    # Split the documents into chunkss
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+
+    # Select which embeddings we want to use
+    embeddings = OpenAIEmbeddings()
+
+    # Create the vector store to use as the index
+    db = Chroma.from_documents(texts, embeddings)
+
+    # Expose this index in a retriever interface
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+
+    return  texts, retriever 
+
+
+def create_qa_chain(retriever):
+    # Create an instance of OpenAI language model
+    llm = OpenAI()
+
+    # Create a RetrievalQA instance with the specified parameters
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="map_reduce",
+        retriever=retriever,
+        return_source_documents=True,
+        verbose=True
+    )
+
+    return qa
+qa = create_qa_chain(retriever)
+# def ask_openai(message):
+#     response = openai.ChatCompletion.create(
+#         model = "gpt-4",
+#         messages=[
+#             {"role": "system", "content": "You are an helpful assistant."},
+#             {"role": "user", "content": message},
+#         ]
+#     )
+    
+#     answer = response.choices[0].message.content.strip()
+#     return answer
+
+# Create your views here.
+@login_required
+def chatbot(request):
+    chats = Chat.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = qa(message)
+        qa("How many contracts are there? List all and describe")
+        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        chat.save()
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html', {'chats': chats})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'register.html', {'error_message': error_message})
+        else:
+            error_message = 'Password dont match'
+            return render(request, 'register.html', {'error_message': error_message})
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+import os
+import sys
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
